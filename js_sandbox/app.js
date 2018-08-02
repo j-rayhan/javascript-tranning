5ba8daf68507d3728e5e1dfc066f25bde82d96e4
johir-rayhan <johir.rayhan01@gmail.com>
2018-08-03 02:46:00 +0600

    Using the Console

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,1 +1,40 @@
-alert("hello horld")
+// alert("hello horld")
+
+/*
+multi
+line 
+comments 
+*/
+
+// Log to console
+
+console.log("hello world"); // text color black
+console.log(124); // color blue
+console.log(true);
+var st  = " Hello ";
+console.log(st);
+console.log([1,2,3,4,5]);
+console.log({a:1, b:2});
+console.table({a:1,b:2});
+
+console.error("This is some error")// color red
+// console.clear();
+
+console.warn('This is warning'); //color orange
+
+console.time("Hello");
+console.log("hello world");
+console.log("hello world");
+
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+
+console.timeEnd('Hello');
+
+
+
+
+
+
