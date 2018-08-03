712c7d9bf25b8ed1eb196639c17853f537152327
johir-rayhan <johir.rayhan01@gmail.com>
2018-08-03 11:51:43 +0600

    String Methods Concatenation

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,23 +1,62 @@
+const firstName = 'Johir';
+const lastName = 'Rayhan';
+const age = 40;
+const str = 'Web design, Web development, Programming';
 let val;
-const num1 = 100;
-const num2 = 50;
 
-// simple math with number;
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
+val = firstName + lastName;
 
-// Math Object
-val = Math.PI ;
-val = Math.E ;
-val = Math.round(2.4) ;
-val = Math.ceil(2.4) ;
-val = Math.floor(2.8) ;
-val = Math.sqrt(64) ;
-val = Math.abs(-3) ;
-val = Math.min(2,33,4,5,-3) ;
-val = Math.max(2,33,4,5,-3) ;
+// Concatenation
+
+val = firstName + " " + lastName;
+
+// Append;
+val = ' Go ';
+val += ' Home';
+
+val = ' Hello, my name is '+ firstName + ' and i am '+ age + ' Old.';
+
+// Escaping
+val = 'That\'s awesom, I can\'t wait';
+// or
+val = "That's awesom, I can't wait";
+
+// Length
+val = firstName.length;
+
+//concat()
+val = firstName.concat(' ', lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[0];
+
+// insexOf()
+val = firstName.indexOf('o');
+val = lastName.lastIndexOf('h');
+
+// charAt()
+val = firstName.charAt('2');
+// get last char
+val = firstName.charAt(firstName.length - 1);
+
+// substring()
+val = firstName.substring(0, 4);
+// slice();
+val = firstName.slice(0,4); //same as substring
+val = firstName.slice(-3); // slice from last
+// split
+val = firstName.split('o');
+val = str.split(',');
+
+// replace()
+val = lastName.replace('Rayhan', 'Hasan');
+
+// includes()
+val = str.includes('Web') // print true
+val = str.includes('Web  ') // print false
+val = str.includes('Go') // print false
 
 console.log(val);
---
+++ js_sandbox/playbook/NumberExample.js
@@ -0,0 +1,29 @@
+let val;
+const num1 = 100;
+const num2 = 50;
+
+// simple math with number;
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+val = Math.PI ;
+val = Math.E ;
+val = Math.round(2.4) ;
+val = Math.ceil(2.4) ;
+val = Math.floor(2.8) ;
+val = Math.sqrt(64) ;
+val = Math.abs(-3) ;
+val = Math.min(2,33,4,5,-3) ;
+val = Math.max(2,33,4,5,-3) ;
+val = Math.random();
+
+val = Math.random() * 20; // Get random from 1 to 20
+val = Math.floor(Math.random() * 20);
+
+
+
+console.log(val);
