5bcb690e707ef0b08a351b1503c07ab3827a29d2
johir-rayhan <johir.rayhan01@gmail.com>
2018-08-03 08:00:21 +0600

    Data Types in javaScript

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,63 +1,65 @@
-// var , let, const
+// Primitive Data Types:
+/*
+1. Stored Directly in the locatoon the variable accesses
+2. Stored on the stack
+*/
 
-// // Init variable
-// var st;
-// console.log(st);
-// st = "Hello";
-// console.log(st);
+/*
+1. String
+2. Number
+3. Boolean
+4. Null          // Read the different in NULL & UNDEFINED
+5. Undefined
+6 Symbols(ES6)
+*/
 
-// // reassign variable
-// var name = 'johir';
-// console.log(name);
-// name = 'raihan';
-// console.log(name);
+//String
+const name = "Johir";
+// Number
+const num = 33;
+// Boolean
+const isOpen = true;
+// Null
+const pan = null; // print Object // for get details google it
+// Undefined
+let startDate;  // It's not const
+// Symbol
+const sym = Symbol();
+// console.log(typeof sym);
 
-// // latters, numbers, _, $
-// // Can't start with number
-// // var 1nam = 'Johi';
-// var _nam = "johirrrr";
-// var $nam = "johirrrr";
 
-// // for regular variable
-// var firstName = 'Johir'; // Camel
-// var first_name = 'Johir'; // Underscore
-// // for ES6 Class
-// var FirstName = 'Johir'; // Pascal
 
-// <---------LET--------
+// Referance Data Types:
+/*
+1. Accessed by reference
+2. Objects that are stored on the heap
+3. A pointer to a location in memory
+*/
+ 
+/*
+1. Arrays
+2. Object Literals
+3. Functions
+4. Dates
+5. Anything Else......
+*/
 
-// // Init variable
-// let st;
-// console.log(st);
-// st = "Hello";
-// console.log(st);
+// Array
+const hobbies = ['movies', 'music', 'sports'];
+// Object literal
+const address = {
+  city: 'Dhaka',
+  state: 'H1'
+}
 
-// // reassign variable
-// let name = 'johir';
-// console.log(name);
-// name = 'raihan';
-// console.log(name);
+const today = new Date();
+console.log(today);
+console.log(typeof today);
 
-// <-----------------CONST-------------
 
-// reassign variable
-const name = 'johir';
-console.log(name);
-//can't reassign
-// name = 'raihan';
+// Types are associated with values not variables
+// The same variable can hold multiple types
+// We do not need to specify types
+// There are supersets of JS and addons to allow static typing(TypeScript, Flow)
 
-const person = {
-  name: 'John',
-  age: 30
-};
-
-person.name = 'Hello';
-person.age= '34';
-
-// console.log(person);
-
-const num = [1,2,3,4,5];
-// num.push(6);
-num.concat(8);
-console.log(num);
-
+// Most other languages are statically typed (java, C#, C++)
---
+++ js_sandbox/playbook/ConsoleExample.js
@@ -0,0 +1,63 @@
+// var , let, const
+
+// // Init variable
+// var st;
+// console.log(st);
+// st = "Hello";
+// console.log(st);
+
+// // reassign variable
+// var name = 'johir';
+// console.log(name);
+// name = 'raihan';
+// console.log(name);
+
+// // latters, numbers, _, $
+// // Can't start with number
+// // var 1nam = 'Johi';
+// var _nam = "johirrrr";
+// var $nam = "johirrrr";
+
+// // for regular variable
+// var firstName = 'Johir'; // Camel
+// var first_name = 'Johir'; // Underscore
+// // for ES6 Class
+// var FirstName = 'Johir'; // Pascal
+
+// <---------LET--------
+
+// // Init variable
+// let st;
+// console.log(st);
+// st = "Hello";
+// console.log(st);
+
+// // reassign variable
+// let name = 'johir';
+// console.log(name);
+// name = 'raihan';
+// console.log(name);
+
+// <-----------------CONST-------------
+
+// reassign variable
+const name = 'johir';
+console.log(name);
+//can't reassign
+// name = 'raihan';
+
+const person = {
+  name: 'John',
+  age: 30
+};
+
+person.name = 'Hello';
+person.age= '34';
+
+// console.log(person);
+
+const num = [1,2,3,4,5];
+// num.push(6);
+num.concat(8);
+console.log(num);
+
